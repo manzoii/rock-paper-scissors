1fe7d232286e56f8db0ddeca608680d37022dcde
manzoii <139693005+manzoii@users.noreply.github.com>
2023-10-09 12:23:36 +0100

    --all

--- README.md
+++ README.md
@@ -3,4 +3,11 @@ it prompts the user for  input
 it randomly selects between the three options for the computer
 it plays the game through a function that takes the user input and the computer choice as paramenters
 it plays the game 5x
-alerts who won 
+alerts who won
+
+new game
+1- initiallizes the playerSelection and the computer selection
+2- the game biggins when the first click of the options is recognized
+3- then the computer selection function runs choosing randomly for the computer
+4- the play function is run to determine who won the round
+5- the game continues until one party has 5 points and is declard the winner
---
+++ script.js
@@ -0,0 +1,88 @@
+const gameState = document.querySelector('div');
+
+const rockbtn = document.createElement('button');
+rockbtn.textContent = 'ROCK';
+rockbtn.setAttribute('id', 'rockbtn'); 
+gameState.appendChild(rockbtn);
+
+const paperbtn = document.createElement("button");
+paperbtn.textContent = 'PAPER';
+paperbtn.setAttribute('id', 'paperbtn'); 
+gameState.appendChild(paperbtn);
+
+const scissorsbtn = document.createElement("button");
+scissorsbtn.textContent = 'BUTTON';
+scissorsbtn.setAttribute('id', 'scissorsbtn'); 
+gameState.appendChild(scissorsbtn);
+
+const gameresult = document.querySelector('p');
+
+let playerScore = 0;
+let computerScore = 0;
+
+document.getElementById("rockbtn").addEventListener("click",rockPlay);
+document.getElementById("paperbtn").addEventListener("click",paperPlay);
+document.getElementById("scissorsbtn").addEventListener("click",scissorsPlay);
+
+function rockPlay(){
+    game('rock');
+}
+function paperPlay(){
+    game('paper');
+}
+function scissorsPlay(){
+    game('scissors');
+}
+
+function game(playerChoice){
+    let playerSelection = playerChoice;
+    function getComputerChoice(){
+        let x = Math.floor((Math.random() * 3) + 1);
+        let computerChoice;
+        if(x===1){
+            computerChoice = 'rock';
+        }else if(x===2){
+            computerChoice='paper';
+        }else{
+            computerChoice='scissors';
+        }
+        return computerChoice;
+    }
+    let computerSelection = getComputerChoice();
+            
+    function playRound(playerSelection, computerSelection) {
+        if(playerSelection==='rock' && computerSelection==='rock'){
+            gameresult.textContent = 'you tie. rock and rock';
+        }else if(playerSelection==='paper' && computerSelection==='paper'){
+            gameresult.textContent = 'you tie. paper and paper';
+        }else if(playerSelection==='scissors' && computerSelection==='scissors'){
+            gameresult.textContent = 'you tie. scissors and scissors';
+        }else if(playerSelection==='rock' && computerSelection==='paper'){
+            computerScore++;
+            gameresult.textContent = 'you lose. rock and paper' ;                       
+        }else if(playerSelection==='paper' && computerSelection==='rock'){
+            playerScore++;
+            gameresult.textContent = 'you win. paper and rock';                       
+        }else if(playerSelection==='rock' && computerSelection==='scissors'){
+            playerScore++;
+            gameresult.textContent = 'you win. rock and scissors';                       
+        }else if(playerSelection==='scissors' && computerSelection==='rock'){
+            computerScore++;
+            gameresult.textContent = 'you lose. scissors and rock';                       
+        }else if(playerSelection==='paper' && computerSelection==='scissors'){
+            computerScore++;
+            gameresult.textContent = 'you lose. paper and scissors';                      
+        }else if(playerSelection==='scissors' && computerSelection==='paper'){
+            playerScore++;
+            gameresult.textContent = 'you win. scissors and paper';
+        }
+    }
+    playRound(playerSelection, computerSelection);
+
+    if(playerScore  === 5){
+        console.log('you won');
+    }else if(computerScore === 5){
+        console.log('you won');
+    }
+
+}
